99fa0ec496a81b49a9f18e5b0717f3814579c8b4
mengweiguo <528663012@qq.com>
2023-11-29 14:24:53 +0800

    添加UnLua模块

--- Source/Kite/Kite.Build.cs
+++ Source/Kite/Kite.Build.cs
@@ -10,7 +10,7 @@ public class Kite : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "UnLua", "Lua" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
