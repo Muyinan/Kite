c9f637c618a741e3eeea3ed8a501c38cdba6ad2e
mengweiguo <528663012@qq.com>
2023-12-02 17:50:35 +0800

    修改BP_KiteCharacter路径

--- Source/Kite/KiteGameModeBase.cpp
+++ Source/Kite/KiteGameModeBase.cpp
@@ -5,11 +5,9 @@
 
 AKiteGameModeBase::AKiteGameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/BP_KiteCharacter"));
-	// UE_LOG(LogTemp, Log, TEXT("~1"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_KiteCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
-		// UE_LOG(LogTemp, Log, TEXT("~2"));
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
